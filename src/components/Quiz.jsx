2733be76e349e622327f4c6bae552b964d1e467a
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-11-01 12:51:28 +0500

    the answer was always the first option. So, i created a shuffled variabe for answers and now the answers are random in the options. Any option can be an answer instead of just the first one.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react'
 import QUESTIONS from '../question.js'
 
-export default function Quiz() {
-    const [userAnswers, setUserAnswers] = useState([])
-    const activeQuestionIndex = userAnswers.length
 
-    function handleSelectAnswer(selectedAnswer) {
-        setUserAnswers((prevUserAnswers) => {
+export default function Quiz() {
+    const [answers, setAnswers] = useState([])
+    const questionIndex = answers.length
+
+    // const shuffledAnswer = QUESTIONS[questionIndex].answers.slice()
+    const shuffledAnswer = [...QUESTIONS[questionIndex].answers]
+    shuffledAnswer.sort(() => Math.random() - 0.5)
+
+    function handleClick(selectedAnswer) {
+        setAnswers((prevAnswer) => {
             return [
-                ...prevUserAnswers, selectedAnswer
+                ...prevAnswer, selectedAnswer
             ]
         })
     }
     return (
         <div id='quiz'>
             <div id='question'>
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <h2>{QUESTIONS[questionIndex].text}</h2>
                 <ul id='answers'>
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswer.map((answer) => (
                         <li key={answer} className='answer'>
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                            <button onClick={handleClick}>{answer}</button>
                         </li>
                     ))}
                 </ul>
@@ -27,3 +32,35 @@ export default function Quiz() {
         </div>
     )
 }
+
+
+// import React, { useState } from 'react'
+// import QUESTIONS from '../question.js'
+
+// export default function Quiz() {
+//     const [userAnswers, setUserAnswers] = useState([])
+//     const activeQuestionIndex = userAnswers.length
+
+//     function handleSelectAnswer(selectedAnswer) {
+//         setUserAnswers((prevUserAnswers) => {
+//             return [
+//                 ...prevUserAnswers, selectedAnswer
+//             ]
+//         })
+//     }
+//     return (
+//         <div id='quiz'>
+//             <div id='question'>
+//                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+//                 <ul id='answers'>
+//                     {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+//                         <li key={answer} className='answer'>
+//                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </div>
+//         </div>
+//     )
+// }
+
